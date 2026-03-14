ad71bd432b76262e291db118078ee0b479a4bfbb
LeKyBa <lekyba2000hc@gmail.com>
2026-03-14 08:23:34 +0700

    chore(FM-165): update ci cd and cors config

---
+++ ai_service/src/main/java/com/familier/ai/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.familier.ai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*", "https://servermonitor-pi.vercel.app"));
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "OPTIONS"));
+        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+        corsConfig.setMaxAge(60L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(source);
+    }
+}
